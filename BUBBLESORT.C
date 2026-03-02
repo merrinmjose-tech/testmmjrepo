cf5a3d6c875f4dfadf6ace2a24255212e14b75de
merrinmjose-tech <merrinmjose@gmail.com>
2026-03-02 11:35:10 +0530

    Create BUBBLESORT.C

---
+++ BUBBLESORT.C
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <time.h>
+
+/* Function to perform Bubble Sort */
+void bubbleSort(int a[], int n)
+{
+    int i, j, temp;
+    for(i = 0; i < n - 1; i++)
+    {
+        for(j = 0; j < n - i - 1; j++)
+        {
+            if(a[j] > a[j + 1])
+            {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
+
+/* Function to perform Selection Sort */
+void selectionSort(int a[], int n)
+{
+    int i, j, minIndex, temp;
+    for(i = 0; i < n - 1; i++)
+    {
+        minIndex = i;
+        for(j = i + 1; j < n; j++)
+        {
+            if(a[j] < a[minIndex])
+                minIndex = j;
+        }
+        temp = a[i];
+        a[i] = a[minIndex];
+        a[minIndex] = temp;
+    }
+}
+
+/* Function to display array */
+void displayArray(int a[], int n)
+{
+    int i;
+    for(i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int a[20], original[20];
+    int n, i;
+    clock_t start, end;
+    double time_taken;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        original[i] = a[i];
+    }
+
+    start = clock();
+    bubbleSort(a, n);
+    end = clock();
+    time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("\nSorted array using Bubble Sort:\n");
+    displayArray(a, n);
+    printf("Time taken by Bubble Sort: %f seconds\n", time_taken);
+
+    for(i = 0; i < n; i++)
+        a[i] = original[i];
+
+    start = clock();
+    selectionSort(a, n);
+    end = clock();
+    time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("\nSorted array using Selection Sort:\n");
+    displayArray(a, n);
+    printf("Time taken by Selection Sort: %f seconds\n", time_taken);
+
+    return 0;
+}
